0597e52a1de18c61aea682b83d4d9e4923983aa5
Tien Nguyen <126621344+mctien04@users.noreply.github.com>
2023-08-02 21:34:03 -0500

    Final

--- index.js
+++ index.js
@@ -22,4 +22,38 @@ function playRound(playerSelection, computerSelection) {
   else {
     return "Lose"
   }
+}
 
+function game() {
+    let player = 0;
+    let computer = 0;
+  
+    for (let round = 1; round <= 5; round++) {
+      const playerSelection = prompt("Enter your choice of rock, paper, or scissors");
+      const computerSelection = getComputerChoice();
+  
+      console.log(playRound(playerSelection, computerSelection));
+  
+      const result = playRound(playerSelection, computerSelection);
+  
+      if (result === "Win") {
+        console.log("You win! " + playerSelection + " beats " + computerSelection);
+        player++;
+      } else if (result === "Lose") {
+        console.log("You lose! " + computerSelection + " beats " + playerSelection);
+        computer++;
+      }
+    }
+    console.log(`Final Score: Player ${player} - ${computer} Computer`);
+
+    // Determine the overall winner
+    if (player > computer) {
+      console.log("Congratulations! You are the overall winner!");
+    } else if (player < computer) {
+      console.log("Computer wins! Better luck next time.");
+    } else {
+      console.log("It's a tie. No overall winner.");
+    }
+  }
+  
+  game();
